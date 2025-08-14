6282e3421812d94dd194a85407a3fde074d2bb63
franciscoqt0 <89044500+franciscoqt0@users.noreply.github.com>
2025-08-13 19:15:27 -0600

    ya corre
    Hace todas las posibilidades binarias, dependiendo de la cantidad de for

--- holaMundo.cpp
+++ holaMundo.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 using namespace std;
-int main() {
-    bool p = false;
-    bool q = false;
 
+void evaluar(bool p, bool q) {
     if ((p || q) && (p || !q)) {
         cout << "Verdadero\n";
     } else {
         cout << "Falso\n";
     }
+}
 
+int main() {
+    for(int i = 0; i <= 1; i++){
+        for(int j = 0; j <= 1; j++){
+            bool p = i; // 0 -> false, 1 -> true
+            bool q = j;
+            cout << "p = " << p << ", q = " << q << " => ";
+            evaluar(p, q);
+        } 
+    }
+    return 0;
 }
